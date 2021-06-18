1b202e7e5d594c3c5df6aeb20bc53c375aca6264
Miguel Á. Sastre <sastre113@gmail.com>
2021-06-19 00:32:55 +0200

    Agregado test

---
+++ src/test/datos/ContenedorDeDatosTest.java
@@ -0,0 +1,104 @@
+/**
+ * 
+ */
+package test.datos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Miguel Á. Sastre <sastre113@gmail.com>
+ * @version 19 jun. 2021, 0:09:06
+ */
+class ContenedorDeDatosTest {
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getDoubleAleatorio()}.
+	 */
+	@Test
+	void testGetDoubleAleatorio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getIntAleatorio()}.
+	 */
+	@Test
+	void testGetIntAleatorio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getIntAleatorio(int)}.
+	 */
+	@Test
+	void testGetIntAleatorioInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getIntAleatorioPositivo()}.
+	 */
+	@Test
+	void testGetIntAleatorioPositivo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getIntAleatorioNegativo(int)}.
+	 */
+	@Test
+	void testGetIntAleatorioNegativo() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getNombreAleatorio()}.
+	 */
+	@Test
+	void testGetNombreAleatorio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getNombreApellidoAleatorio()}.
+	 */
+	@Test
+	void testGetNombreApellidoAleatorio() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getListNombresAleatorios()}.
+	 */
+	@Test
+	void testGetListNombresAleatorios() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getListNombresApellidosAleatorios()}.
+	 */
+	@Test
+	void testGetListNombresApellidosAleatorios() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getListNombresAleatorios(int)}.
+	 */
+	@Test
+	void testGetListNombresAleatoriosInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link resource.datos.ContenedorDeDatos#getListNombresApellidosAleatorios(int)}.
+	 */
+	@Test
+	void testGetListNombresApellidosAleatoriosInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
