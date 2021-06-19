d9ec4d89cafc4694fc90111efcb1c61841404db7
Miguel Á. Sastre <sastre113@gmail.com>
2021-06-19 12:39:18 +0200

    Actualizado javadoc y añadido comentarios.
    Prueba con javadoc

--- src/resource/datos/ContenedorDeDatos.java
+++ src/resource/datos/ContenedorDeDatos.java
@@ -12,60 +12,116 @@ import java.util.Random;
  * @author Miguel Á. Sastre <sastre113@gmail.com>
  * @version 18 jun. 2021, 20:43:05
  */
-
 public final class ContenedorDeDatos {
 
+	/**
+	 * Constante que contiene el tamaño máximo de las listas generadas aleatoriamente.
+	 */
 	private final int MAX_VALOR_LISTA = 100;
 	
+	/**
+	 * Constante que contiene un simple texto.
+	 */
+	private final String texto = Messages.getString("ContenedorDeDatos.0"); //$NON-NLS-1$
+	
+	/**
+	 * Variable que contiene una lista con nombres y dos apellidos.
+	 */
 	private final List<String> listaNombresApellidos = new ArrayList<>(
-			Arrays.asList("Áurea Gabriel Quijano", "Ovidia Flavio Arriola", "Violeta Adelia Ojeda",
-					"Norberto Margarita Domínguez", "María José Rosa María Villa", "Aurora Valeria Casales",
-					"Silvio Rodolfo Machado", "Donato Dolores Figueroa", "Rosaura Pastor Ureña", "Justino Sofía Araujo",
-					"José Antonio Maricruz Villaverde", "Ana María Marciano Márquez", "José Ángel Ariel Roldán",
-					"Francisco Javier Alejandra María", "Mireya Vinicio Muñoz", "Rosalva Regina Torres",
-					"Norberto Kike Iglesias", "Heriberto Héctor Pantoja", "Pacífica Jennifer Contreras",
-					"Ginés Ofelia Basurto"));
-	private final List<String> listaNombres = new ArrayList<>(Arrays.asList("Bernabé ", "Yolanda", "Lorena", "Nacho",
-			"Isaías", "Aitana", "José ", "Angélia", "Catalina", "Sabina", "Raimundo", "Jorge", "Abril", "Ismael",
-			"Alexis", "Adelardo", "Fernando", "Rolando", "Matías", "Jenifer"));
+			Arrays.asList("Áurea Gabriel Quijano", "Ovidia Flavio Arriola", "Violeta Adelia Ojeda", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					"Norberto Margarita Domínguez", "María José Rosa María Villa", "Aurora Valeria Casales", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					"Silvio Rodolfo Machado", "Donato Dolores Figueroa", "Rosaura Pastor Ureña", "Justino Sofía Araujo", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+					"José Antonio Maricruz Villaverde", "Ana María Marciano Márquez", "José Ángel Ariel Roldán", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					"Francisco Javier Alejandra María", "Mireya Vinicio Muñoz", "Rosalva Regina Torres", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					"Norberto Kike Iglesias", "Heriberto Héctor Pantoja", "Pacífica Jennifer Contreras", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+					"Ginés Ofelia Basurto")); //$NON-NLS-1$
+	/**
+	 * Variable que contiene una lista de nombres.
+	 */
+	private final List<String> listaNombres = new ArrayList<>(Arrays.asList("Bernabé ", "Yolanda", "Lorena", "Nacho", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+			"Isaías", "Aitana", "José ", "Angélia", "Catalina", "Sabina", "Raimundo", "Jorge", "Abril", "Ismael", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$ //$NON-NLS-10$
+			"Alexis", "Adelardo", "Fernando", "Rolando", "Matías", "Jenifer")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
 	
 	
+	/**
+	 * Método que genera un número decimal. 
+	 * @return
+	 */
 	public double getDoubleAleatorio() {
 		return new Random().nextDouble();
 	}
 	
+	/**
+	 * Método que genera un número entero.
+	 * @return
+	 */
 	public int getIntAleatorio() {
 		return new Random().nextInt();
 	}
 	
+	/**
+	 * Método que genera un número con un rango máximo establecido.
+	 * @param maxValor
+	 * @return
+	 */
 	public int getIntAleatorio(int maxValor) {
 		return new Random().nextInt(maxValor);
 	}
 	
+	/**
+	 * Método que genera un número entero positivo. 
+	 * @return
+	 */
 	public int getIntAleatorioPositivo() {
 		return Math.abs(this.getIntAleatorio());
 	}
 	
+	/**
+	 * Método que genera un número entero negativo.
+	 * @param maxValor
+	 * @return
+	 */
 	public int getIntAleatorioNegativo(int maxValor) {
 		return -this.getIntAleatorio(maxValor);
 	}
 	
+	/**
+	 * Método que devuelve un nombre.
+	 * @return
+	 */
 	public String getNombreAleatorio() {
 		return this.getListaNombres().get(this.getIndiceAleatorioListaNombre());
 	}
 	
+	/**
+	 * Método que devuelve un nombre y apellidos.
+	 * @return
+	 */
 	public String getNombreApellidoAleatorio() {
 		return this.getListaNombresApellidos().get(this.getIndiceAleatorioListaNombreConApellido());
 	}
 
+	/**
+	 * Método que devuelve una lista con nombres.
+	 * @return
+	 */
 	public List<String> getListNombresAleatorios(){
 		return this.getListNombresAleatorios(this.getIntAleatorio(MAX_VALOR_LISTA));
 	}
 	
+	/**
+	 * Método que devuelve una lista con nombres y apellidos.
+	 * @return
+	 */
 	public List<String> getListNombresApellidosAleatorios(){
 		return this.getListNombresApellidosAleatorios(this.getIntAleatorio(MAX_VALOR_LISTA));
 	} 
 	
+	/**
+	 * Método que devuelve una lista del tamaño pasado que contiene nombres.
+	 * @param tamañoLista
+	 * @return
+	 */
 	public List<String> getListNombresAleatorios(int tamañoLista){
 		List<String> list = new ArrayList<>();
 		
@@ -76,6 +132,11 @@ public final class ContenedorDeDatos {
 		return list;
 	}
 	
+	/**
+	 * Método que devuelve una lista del tamaño pasado que contiene nombres y apellidos.
+	 * @param tamañoLista
+	 * @return
+	 */
 	public List<String> getListNombresApellidosAleatorios(int tamañoLista){
 		List<String> list = new ArrayList<>();
 		
@@ -86,6 +147,13 @@ public final class ContenedorDeDatos {
 		return list;
 	} 
 	
+	/**
+	 * Método 'getter' que devuelve el texto.
+	 * @return
+	 */
+	public String getTexto() {
+		return this.texto;
+	}
 	
 	/*
 	 * Métodos auxiliares de la clase
---
+++ src/resource/datos/Messages.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package resource.datos;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+
+/**
+ * @author Miguel
+ *
+ */
+public class Messages {
+	private static final String BUNDLE_NAME = "resource.messages"; //$NON-NLS-1$
+
+	private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+	private Messages() {
+	}
+
+	public static String getString(String key) {
+		try {
+			return RESOURCE_BUNDLE.getString(key);
+		} catch (MissingResourceException e) {
+			return '!' + key + '!';
+		}
+	}
+}
