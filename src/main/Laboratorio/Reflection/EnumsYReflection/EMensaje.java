4dd2ed72c7ba5787f80c651b1d18e63d9fc41a2d
Miguel Á. Sastre <sastre113@gmail.com>
2021-07-13 00:20:57 +0200

    Proxima implementación de Enums con Reflection.

---
+++ src/main/Laboratorio/Reflection/EnumsYReflection/EMensaje.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package main.Laboratorio.Reflection.EnumsYReflection;
+
+/**
+ * @author Miguel Á. Sastre <sastre113@gmail.com>
+ * @version 13 jul. 2021, 0:13:26
+ */
+public enum EMensaje {
+
+}
---
+++ src/main/Laboratorio/Reflection/EnumsYReflection/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package main.Laboratorio.Reflection.EnumsYReflection;
+
+/**
+ * @author Miguel Á. Sastre <sastre113@gmail.com>
+ * @version 13 jul. 2021, 0:13:05
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
