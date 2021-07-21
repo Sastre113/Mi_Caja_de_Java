a09f8690c21ce2317c00a747b444d2809ff582bf
Miguel Á. Sastre <sastre113@gmail.com>
2021-07-21 09:24:44 +0200

    Implementación EnumsYReflection.

--- src/main/Laboratorio/Reflection/EnumsYReflection/EMensaje.java
+++ src/main/Laboratorio/Reflection/EnumsYReflection/EMensaje.java
@@ -14,8 +14,29 @@ public enum EMensaje {
 	
 	// Genéricas
 	PT_EXITO	(0,"info.exito"),
-	PT_VACIA	(1,"error.peticion.vacia"),
-	PT_FRACASO	(2,"error.peticion.fracaso");
+	PT_VACIA	(1000,"error.peticion.vacia"),
+	ALTA_EMAIL_NO_VALIDO (1001,"error.alta.emailNoValido"),
+	ALTA_NIF_NO_VALIDO (1002,"error.alta.nifNoValido"),
+	BANK_EMAIL_DOCUMENTO_DUPLICADO (1003,"error.bank.documentoDuplicado"),
+	USUARIO_ERROR_AUTENTIFICACION (1004,"error.usuario.autenticacioErronea"),
+	PT_FRACASO	(2000,"error.peticion.fracaso");
+	
+	
+
+	/** Identificador del error */
+	private int id;
+	/** Nombre de variable que almacena su descripción*/
+	private String value;
+	/** ECodigo correspondiente al id*/
+	private ECodigo codigo;
+	
+	/** OK: 0 - 999*/
+	private final int ID_OK_MAX = 1000;
+	/** EV: 1000 - 1999*/
+	private final int ID_EV_MAX = 2000;
+	/** KO: 2000 - 2999*/
+	private final int ID_KO_MAX = 3000;
+	
 	
 	/**
 	 * Constructor
@@ -26,31 +47,27 @@ public enum EMensaje {
 	private EMensaje(int id, String value) {
 		this.id = id;
 		this.value = value;
+		this.codigo = this.getECodigo(id);
 	}
-
-	/** Identificador del error */
-	private int id;
-	/** Nombre de variable que almacena su descripción*/
-	private String value;
 
 	/**
 	 * Método que devuelve el ECodigo que le corresponde al EMensaje dependiendo
 	 * de su id
 	 * 
-	 * @return cadena de texto
+	 * @return ECodigo 
 	 */
-	public String getECodigo() {
-		String msg;
+	private ECodigo getECodigo(int id) {
+		ECodigo eCodigo;
 		
-		if(this.getId() == 0) {
-			msg = ECodigo.OK.toString();
-		} else if(this.getId() == 1) {
-			msg = ECodigo.EV.toString();
+		if(id < ID_OK_MAX) {
+			eCodigo = ECodigo.OK;
+		} else if (id < ID_EV_MAX) {
+			eCodigo = ECodigo.EV;
 		} else {
-			msg = ECodigo.KO.toString();
+			eCodigo = ECodigo.KO;
 		}
 		
-		return msg;
+		return eCodigo;
 	}
 	
 	/**
@@ -77,7 +94,7 @@ public enum EMensaje {
 	 * @return mensaje construido con el ECodigo y su id
 	 */
 	private String getCodigoError() {
-		return String.format("%s_%s", this.getECodigo(),this.getId());
+		return String.format("%s_%s", this.getCodigo() ,this.getId());
 	}
 	
 	/**
@@ -99,17 +116,24 @@ public enum EMensaje {
 	
 	/**
 	 * 
-	 * @return String 
-	 */
-	private String getValue() {
-		return value;
-	}
-
-	/**
-	 * 
 	 * @return int
 	 */
 	private int getId() {
 		return id;
+	}
+	
+	/**
+	 * @return the eCodigo
+	 */
+	private ECodigo getCodigo() {
+		return codigo;
+	}
+	
+	/**
+	 * 
+	 * @return String 
+	 */
+	private String getValue() {
+		return value;
 	}
 }
--- src/main/Laboratorio/Reflection/EnumsYReflection/Main.java
+++ src/main/Laboratorio/Reflection/EnumsYReflection/Main.java
@@ -18,18 +18,21 @@ public class Main {
 	
 	
 	static void level1() {
+		System.out.println(String.format("%s", EMensaje.ALTA_NIF_NO_VALIDO.getMsg()));
+		System.out.println(String.format("%s", EMensaje.ALTA_EMAIL_NO_VALIDO.getMsg()));
 		level2();
 	}
 	
 	static void level2() {
+		System.out.println(String.format("%s", EMensaje.BANK_EMAIL_DOCUMENTO_DUPLICADO.getMsg()));
 		level3();
 	}
 	
 	static void level3() {
-		
 		System.out.println(String.format("%s", EMensaje.PT_EXITO.getMsg()));
+		System.out.println(String.format("%s", EMensaje.PT_VACIA.getMsg()));
+		System.out.println(String.format("%s", EMensaje.USUARIO_ERROR_AUTENTIFICACION.getMsg()));
 		System.out.println(String.format("%s", EMensaje.PT_FRACASO.getMsg()));
-		
 	}
 
 }
