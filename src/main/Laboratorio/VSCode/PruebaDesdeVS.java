6dd0a0116138e31c873880a8d56306a3865fb97c
Miguel Á. Sastre <sastre113@gmail.com>
2022-02-27 11:27:14 +0100

    prueba

--- src/main/Laboratorio/VSCode/PruebaDesdeVS.java
+++ src/main/Laboratorio/VSCode/PruebaDesdeVS.java
@@ -1,6 +1,7 @@
 package main.Laboratorio.VSCode;
 
 class PruebaDesdeVS{
+    // Comentario de prueba
     public static void main(String[] args) {
         System.out.println("Hello VSCode!!!");
     }
