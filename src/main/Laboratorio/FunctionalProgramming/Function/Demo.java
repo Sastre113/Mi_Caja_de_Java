92c7590a12b4226eaf77e10b23a3b57c9644fead
Miguel Á. Sastre <sastre113@gmail.com>
2022-09-18 01:13:41 +0200

    Pruebas con Supplier y Function.

--- src/main/Laboratorio/FunctionalProgramming/Function/Demo.java
+++ src/main/Laboratorio/FunctionalProgramming/Function/Demo.java
@@ -5,6 +5,7 @@ package main.Laboratorio.FunctionalProgramming.Function;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 
 /**
  * @author Miguel Á. Sastre <sastre113@gmail.com>
@@ -18,7 +19,24 @@ public class Demo {
 		listaFunciones.add(EFunciones.FX);
 		listaFunciones.add(EFunciones.HX);
 		listaFunciones.forEach(funcion -> funcion.ejecutar(null));
+		
+		Function<String, Integer> miFunction = new Function<>() {
+			@Override
+			public Integer apply(String t) {
+				return t.equals("pepe") ? 100 : 50;
+			}
+		};
+		
+		Function<String, Integer> miFunctionLambda = t -> t.equals("pepe") ? 100 : 50;
+		Function<String, Integer> miFunctionMethodReference = Demo::getMiFunction;
+		
+		
+		System.out.println("Usando mi función con el parámetro: 'pepe' , Resultado: " + miFunction.apply("pepe"));
+		System.out.println("Usando mi función con el parámetro: 'pepon' , Resultado: " + miFunction.apply("pepon"));
+			
 	}
-	
 
+	private static Integer getMiFunction(String string1) {
+		return string1.equals("pepe") ? 100 : 50;
+	}
 }
--- src/main/Laboratorio/FunctionalProgramming/Supplier/Demo.java
+++ src/main/Laboratorio/FunctionalProgramming/Supplier/Demo.java
@@ -3,18 +3,29 @@
  */
 package main.Laboratorio.FunctionalProgramming.Supplier;
 
+import java.util.function.Supplier;
+
 /**
  * @author Miguel Á. Sastre <sastre113@gmail.com>
  * @version 13 sept 2022, 19:59:22
  */
 public class Demo {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Supplier<String> miSupplier = new Supplier<String>() {
 
+			@Override
+			public String get() {
+				return "pepe";
+			}
+		};
+		
+		Supplier<String> miSupplierLambda = () -> "pepe";
+		Supplier<String> miSupplierMethodReference = Demo::miMetodoSupplier;
+	}
+
+	private static String miMetodoSupplier() {
+		return "pepe";
 	}
 
 }
