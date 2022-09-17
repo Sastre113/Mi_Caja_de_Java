e913da7260be7c5fc736d7e11e5eea47e453aeff
Miguel Á. Sastre <sastre113@gmail.com>
2022-09-17 23:58:54 +0200

    Pruebas con Predicate

--- src/main/Laboratorio/FunctionalProgramming/Predicate/Demo.java
+++ src/main/Laboratorio/FunctionalProgramming/Predicate/Demo.java
@@ -3,6 +3,8 @@
  */
 package main.Laboratorio.FunctionalProgramming.Predicate;
 
+import java.util.function.Predicate;
+
 /**
  * @author Miguel Á. Sastre <sastre113@gmail.com>
  * @version 13 sept 2022, 19:59:22
@@ -14,7 +16,17 @@ public class Demo {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Predicate<String> miPredicate = t -> t.equals("pepe");
+		Predicate<String> miPredicateMethodReference = Demo::miFuncionPredicate;
+		
+		System.out.println("Ejecutando miPredicate: " + miPredicate.test("pepe"));
+		System.out.println("Ejecutando miPredicate: " + miPredicate.test("pepon"));
+		System.out.println("Ejecutando miPredicateMethodReference: " + miPredicateMethodReference.test("pepe"));
+		System.out.println("Ejecutando miPredicateMethodReference: " + miPredicateMethodReference.test("pepon"));
+	}
+	
+	private static boolean miFuncionPredicate(String str) {
+		return str.equals("pepon");
 	}
 
 }
